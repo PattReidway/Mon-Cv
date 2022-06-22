1ab3d956c42467f9ab90ec8a9507349afad34176
benjamin ebran <Ebran.b@gmail.com>
2022-06-22 16:39:36 +0200

    noIdea

---
+++ Javascript/javascript.js
@@ -0,0 +1,7 @@
+const openButton = document.querySelector(".open-profil")
+const closeButton = document.querySelector(".close-profil")
+const profil = document.querySelectorAll(".profil")
+ openButton.addEventListener("click", function(event){
+
+
+ })
